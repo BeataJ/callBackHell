0d25191f53e2e2ef4dab39768b7941db4276b3db
beata <beata@emc2wave.com>
2020-01-24 21:49:30 -0800

    create moveX()

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const btn = document.querySelector("button");
 
-setTimeout(() => {
-  btn.style.transform = `translateX(100px)`;
-}, 3000);
+// setTimeout(() => {
+//   btn.style.transform = `translateX(100px)`;
+//   setTimeout(() => {
+//     btn.style.transform = `translateX(200px)`;
+//     setTimeout(() => {
+//       btn.style.transform = `translateX(300px)`;
+//       setTimeout(() => {
+//         btn.style.transform = `translateX(400px)`;
+//         setTimeout(() => {
+//           btn.style.transform = `translateX(500px)`;
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const moveX = (element, amount, delay) => {
+  setTimeout(() => {
+    element.style.transform = `translateX(${amount}px)`;
+  }, delay);
+};
+
+moveX(btn, 600, 2000);
