ea64ce3e31dae3b751b4a87c229ee41f2d5c0b70
beata <beata@emc2wave.com>
2020-01-24 21:59:30 -0800

    add callback

--- app.js
+++ app.js
@@ -19,10 +19,16 @@ const btn = document.querySelector("button");
 const moveX = (element, amount, delay, callback) => {
   setTimeout(() => {
     element.style.transform = `translateX(${amount}px)`;
-    callback();
+    if (callback) callback();
   }, delay);
 };
 
 moveX(btn, 100, 2000, () => {
-  moveX(btn, 400, 1000);
+  moveX(btn, 200, 1000, () => {
+    moveX(btn, 300, 1000, () => {
+      moveX(btn, 400, 1000, () => {
+        moveX(btn, 500, 1000);
+      });
+    });
+  });
 });
